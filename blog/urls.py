da1652784be87c3f633ba3929a4c5ee56c5901aa
Swarup Sarangi <swarupsarangi113@gmail.com>
2020-08-29 09:15:32 +0530

    added pagination feature

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.PostListView.as_view(template_name='blog/home.html'),name='blog-home'),
+    path('user/<str:username>/',views.UserPostListView.as_view(),name='user-posts'),
     path('post/<int:pk>/',views.PostDetailView.as_view(),name='post-detail'), 
     path('post/new/',views.PostCreateView.as_view(),name='post-create'), 
     path('post/<int:pk>/update/',views.PostUpdateView.as_view(),name='post-update'),
